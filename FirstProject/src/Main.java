bb473e5697aa947d6add1f6ec34e5fdb297372ba
Nike Balonga <rnnbalongapro@gmail.com>
2023-08-04 16:20:31 +0900

    First project for Java

---
+++ FirstProject/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// println will print a string and add a line break or just use \n
+		// \t will add an indent
+		System.out.println("This is Java.");
+		System.out.print("\t This is interesting.");
+		
+		/*
+		 * I can also write comments like these.
+		 * Pretty handy for writing multiple lines of comments.
+		 * 
+		 */
+	}
+
+}
