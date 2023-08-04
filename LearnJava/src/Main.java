cf8e614f1008ca584ac58edbad720bff819e1b37
Nike Balonga <rnnbalongapro@gmail.com>
2023-08-04 22:27:31 +0900

    Add Starting files for LearnJava project

---
+++ LearnJava/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Always put the variable type
+        int int_1 = 10;
+        int int_2 = 20;
+        int int_3 = 50;
+        int int_4 = 100;
+
+        int sum = int_1 + int_2 + int_3 + int_4;
+        System.out.println("Sum is: " + sum);
+    }
+}
+
