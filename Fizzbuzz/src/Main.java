907ddb533a54255194c7e187bac0a85a61758a3d
Nike Balonga <rnnbalongapro@gmail.com>
2023-08-09 17:18:06 +0900

    Build out Fizzbuzz

--- Fizzbuzz/src/Main.java
+++ Fizzbuzz/src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Put ending number: ");
+        int end_num = scanner.nextInt();
+        scanner.nextLine();
+
+        for (int i=1; i<=end_num; i++) {
+            if (i % 3 == 0 && i % 5 ==0) {
+                System.out.println("Fizzbuzz");
+            } else if (i % 3 == 0 ) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0 ) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
     }
 }
