586d1f758e537507dd4e38320768f84ac1f7987d
Nike Balonga <rnnbalongapro@gmail.com>
2023-12-27 21:33:32 +0900

    Complete SecondsConverter problem.

--- thinkjavalessons/src/SecondsConverter.java
+++ thinkjavalessons/src/SecondsConverter.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class SecondsConverter {
+import java.util.Scanner;
+public class SecondsConverter {
+    public static void main (String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter number of seconds: ");
+        //Capture user input
+        int userSeconds = in.nextInt();
+        int hours = userSeconds / 3600;
+        int minutes = (userSeconds % 3600) / 60;
+        int seconds = userSeconds % 60;
+        System.out.println("Hours: " + hours);
+        System.out.println("Minutes: " + minutes);
+        System.out.println("Seconds: " + seconds);
+    }
 }
