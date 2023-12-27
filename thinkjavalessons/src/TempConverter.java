2ecef7f9cc579b015148040c64b4c820726ed2d0
Nike Balonga <rnnbalongapro@gmail.com>
2023-12-27 21:09:50 +0900

    Finish coding class TempConverter. Improvements to follow.

--- thinkjavalessons/src/TempConverter.java
+++ thinkjavalessons/src/TempConverter.java
@@ -3,10 +3,12 @@ import java.util.Scanner;
 public class TempConverter {
     public static void main (String[] args){
         Scanner in = new Scanner(System.in);
-        System.out.print("What's the temperature in Celcius?: ");
+        System.out.print("Enter a temperature in Celcius: ");
         //Capture user input as a double instead of an int
         double userTemp = (double) in.nextInt();
-       
+        double convertedTemp = (userTemp * 9 / 5) + 32;
+        System.out.println();
+        System.out.println(userTemp + "C = " + convertedTemp + " F");
     }
 
 }
