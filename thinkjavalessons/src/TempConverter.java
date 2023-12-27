c962fb5413c7c58d45c327dc2a6707473fcf12e9
Nike Balonga <rnnbalongapro@gmail.com>
2023-12-27 21:03:14 +0900

    Add thinkjavalessons package

---
+++ thinkjavalessons/src/TempConverter.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class TempConverter {
+    public static void main (String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.print("What's the temperature in Celcius?: ");
+        //Capture user input as a double instead of an int
+        double userTemp = (double) in.nextInt();
+       
+    }
+
+}
