7ee4c130c74146edfc528fd110bc95af856f08b4
Nike Balonga <rnnbalongapro@gmail.com>
2024-01-05 22:06:19 +0900

    Create methods getUserNumber and getDifference

--- thinkjavalessons/src/NumberGuessingGame.java
+++ thinkjavalessons/src/NumberGuessingGame.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.util.Random;
+import java.lang.Math;
 
 public class NumberGuessingGame {
     //Create randomizer
@@ -9,10 +10,26 @@ public class NumberGuessingGame {
         //Set maximum and minimum values to be randomly generated
         int maxNumber = 100;
         int minNumber = 1;
-        int x = rand.nextInt((maxNumber - minNumber) + minNumber);
+
+        //Randomize number
+        int randNumber = rand.nextInt((maxNumber - minNumber) + minNumber);
         System.out.println("This is the number guessing game.");
-        System.out.print("Please select a number from 1-100: ");
-        //Get input from user
+
+
+        //Ask for User Number
         int userNumber = in.nextInt();
+
+        //Set number of tries user can guess
+        int userTries = 5;
+        int numDifference = Math.abs(userNumber - randNumber);
+    }
+
+    public static int getUserNumber(int userNumber){
+        System.out.print("Please select a number from 1-100: ");
+        return 0;
+    }
+
+    public static int getDifference(int randNum, int userNum){
+        return Math.abs(userNum - randNum);
     }
 }
